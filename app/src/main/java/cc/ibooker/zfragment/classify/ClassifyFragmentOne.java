115a7c6b6a4897a96225606f082c7b23d19e8765
zrunker <zrunker@yahoo.com>
2018-03-03 19:41:30 +0800

    init Commit

--- app/src/main/java/cc/ibooker/zfragment/classify/ClassifyFragmentOne.java
+++ app/src/main/java/cc/ibooker/zfragment/classify/ClassifyFragmentOne.java
@@ -18,7 +18,6 @@ public class ClassifyFragmentOne extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_classify_one, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_classify_one, container, false);
     }
 }
